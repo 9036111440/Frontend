08a77d3efc76921cfd6088155fc8b0edbe805f74
Praveen Kumar sekar <praveenkumars1598@gmail.com>
2026-08-16 19:38:05 +0530

    changed env

--- frontend-app/src/app/environments/environment.ts
+++ frontend-app/src/app/environments/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:3000/api'
+// };
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://backend-1igb.onrender.com/api'
 };
